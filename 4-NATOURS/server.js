f37f638f68b520c10fe60356a005725015765773
JamesOlaleye <olanipekunjamesolaleye@gmail.com>
2024-01-10 04:33:38 +0100

    config eslint rules, clean code

--- 4-NATOURS/server.js
+++ 4-NATOURS/server.js
@@ -3,8 +3,6 @@ const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-// console.log(process.env);
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
