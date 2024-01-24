b6d2356b55b540e6b05da9df732e788c6b2bbbff
JamesOlaleye <olanipekunjamesolaleye@gmail.com>
2024-01-24 12:31:15 +0100

    Mongoose mastery

--- 4-NATOURS/server.js
+++ 4-NATOURS/server.js
@@ -1,9 +1,17 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
+// Connect to DB and start server
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`App running on port ${port}...`);
+      console.log('DB connection successful');
+    });
+  })
+  .catch((err) => console.log(err));
