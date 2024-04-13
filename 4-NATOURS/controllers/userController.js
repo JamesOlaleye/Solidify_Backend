6a01aab9f5aa12688dacbc1a7aeef9569ee5a915
JamesOlaleye <olanipekunjamesolaleye@gmail.com>
2024-04-13 20:25:58 +0100

    Adding a me endpoint

--- 4-NATOURS/controllers/userController.js
+++ 4-NATOURS/controllers/userController.js
@@ -11,6 +11,11 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
@@ -47,8 +52,6 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
     data: null,
   });
 });
-
-
 
 exports.createUser = (req, res) => {
   res.status(500).json({
