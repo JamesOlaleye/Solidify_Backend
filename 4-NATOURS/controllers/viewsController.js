d8734a7df9ae2ee568476e307bcf65d797b8f17b
JamesOlaleye <olanipekunjamesolaleye@gmail.com>
2024-04-26 07:46:03 +0100

    Building the Tour Page Part-1

--- 4-NATOURS/controllers/viewsController.js
+++ 4-NATOURS/controllers/viewsController.js
@@ -2,21 +2,28 @@ const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
-  // Get tour data from collection
+  // 1) Get tour data from collection
   const tours = await Tour.find();
 
-  // Build template
-
-  //Render that templateusing tour data from
-
+  // 2) Build template
+  // 3) Render that templateusing tour data from 1)
   res.status(200).render('overview', {
     title: 'All Tours',
     tours,
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  // 1) get the data for the requested tour (include reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+
+  // 2) Build template
+  // 3) Render that templateusing tour data from 1)
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour,
   });
-};
+});
